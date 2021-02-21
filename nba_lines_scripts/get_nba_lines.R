cc01ee4679bb54cc8ae9eb7e99c3bba4aeeea3ca
jimtheflash <jim.kloet@gmail.com>
2021-02-21 12:10:17 -0600

    update latest games

--- nba_lines_scripts/get_nba_lines.R
+++ nba_lines_scripts/get_nba_lines.R
@@ -2,7 +2,7 @@
 library(bettoR)
 output_dir <- '/Users/jim/Desktop/nba_lines/'
 # start_date <- as.Date('2015-01-01')
-start_date <- as.Date('2018-06-01')
+start_date <- as.Date('2020-12-24')
 end_date <- Sys.Date()
 date_range <- gsub('-', '', as.character(seq.Date(start_date, end_date, by = "day")))
 bet_types <- c("spread", "total", "moneyline")
