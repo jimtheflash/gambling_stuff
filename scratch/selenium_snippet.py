9cd59ad4403ebdc0d3e9f21d126e7919bb130450
jimtheflash <jim.kloet@gmail.com>
2021-02-22 14:42:52 -0600

    update gamelogs and pbp, add scratch folder with selenium snippet, update gitignore

---
+++ scratch/selenium_snippet.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+
+import os
+import time
+
+driver = webdriver.Chrome(executable_path = "/Users/jim/Documents/drivers/chromedriver 2")
+# driver.get('https://classic.sportsbookreview.com/betting-odds/nba-basketball/money-line/?date=20210201')
+driver.get('https://www.lineups.com/nba/nba-fantasy-basketball-projections')
+
+classname.click()
+/html/body/app-root/div[3]/app-fantasy-projections/div/div/div[4]/div/div[1]/div[4]/div[2]/div/app-dropdown/div/div/div[3]/div/label
