16284d1c466cba40a642c019899120a79adaa522
jimtheflash <jim.kloet@gmail.com>
2021-02-26 08:42:32 -0600

    update nba games, pbp, boxscores; add nfl odds from 2015-present

---
+++ nba_lines_scripts/get_nfl_lines.R
@@ -0,0 +1,30 @@
+library(bettoR)
+
+output_dir <- './data/nfl_lines/'
+start_date <- as.Date('2015-01-01')
+# start_date <- as.Date('2021-02-01')
+end_date <- Sys.Date()
+date_range <- gsub('-', '', as.character(seq.Date(start_date, end_date, by = "day")))
+bet_types <- c("spread", "total", "moneyline")
+message(Sys.time(), " initializing loop")
+for (d in date_range) {
+  message(Sys.time(), " trying to grab NFL lines for date ", d)
+  for (b in bet_types) {
+    message("getting ", b)
+    Sys.sleep(3)
+    line_info <- try(
+      get_lines(
+        sport = "NFL",
+        bet_type = b,
+        start_date = d
+      )
+    )
+    if ('try-error' %in% class(line_info)) {
+      message(Sys.time(), " something went amiss trying to scrape ", b, " for date ", d)
+      next
+    } else {
+      output_string <- paste0(output_dir, 'nfl_', d, '_', b, '.csv')
+      write.csv(line_info, output_string, row.names = FALSE, quote = FALSE)
+    }
+  }
+}
