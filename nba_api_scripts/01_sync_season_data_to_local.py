b7483b3045091ca31262e482da4cd598bf5ce3fd
John Schroeder <4474856+johnlovesdata@users.noreply.github.com>
2021-02-28 09:50:37 -0600

    add boxscore to local sync

--- nba_api_scripts/01_sync_season_data_to_local.py
+++ nba_api_scripts/01_sync_season_data_to_local.py
@@ -7,7 +7,7 @@ from glob import glob
 import numpy as np
 
 from args import get_cli_args
-from game import get_game_pbp
+from game import get_game_pbp, get_game_boxscore
 from get_logger import get_logger
 from season import format_season_string, get_current_season, get_season_gamelog
 
@@ -29,6 +29,10 @@ BS_PATH = os.path.join(
     "data",
     "nba_boxscores",
 )
+SYNC_CATEGORIES = [
+    {"category": "play_by_play", "base_path": PBP_PATH, "function": get_game_pbp},
+    {"category": "boxscore", "base_path": BS_PATH, "function": get_game_boxscore},
+]
 
 
 def main(args: dict):
@@ -40,17 +44,22 @@ def main(args: dict):
     gamelog_df.to_csv(source_file_name, index=False)
     game_ids = list(gamelog_df.GAME_ID.unique())
 
-    logger.info("Syncing season PBP files to local")
-    pbp_files = glob(os.path.join(PBP_PATH, "*.csv"))
-    pbp_game_ids = [os.path.basename(x) for x in pbp_files]
-    missing_game_ids = list(np.setdiff1d(game_ids, pbp_game_ids))
-    logger.info(f"There are {len(missing_game_ids)} missing games to retrieve")
-    if not args.get("dryrun"):
-        for game_id in missing_game_ids:
-            pbp_df = get_game_pbp(game_id)
-            pbp_file_name = os.path.join(PBP_PATH, f"{game_id}.csv")
-            pbp_df.to_csv(pbp_file_name, index=False)
-            time.sleep(2)
+    for sync in SYNC_CATEGORIES:
+        category = sync["category"]
+        base_path = sync["base_path"]
+        func = sync["function"]
+
+        logger.info(f"Syncing season {category} files to local")
+        sync_files = glob(os.path.join(base_path, "*.csv"))
+        local_game_ids = [os.path.basename(x) for x in sync_files]
+        missing_game_ids = list(np.setdiff1d(game_ids, local_game_ids))
+        logger.info(f"There are {len(missing_game_ids)} missing games to retrieve")
+        if not args.get("dryrun"):
+            for game_id in missing_game_ids[:5]:
+                pbp_df = func(game_id)
+                pbp_file_name = os.path.join(PBP_PATH, f"{game_id}.csv")
+                pbp_df.to_csv(pbp_file_name, index=False)
+                time.sleep(2)
 
 
 if __name__ == "__main__":
