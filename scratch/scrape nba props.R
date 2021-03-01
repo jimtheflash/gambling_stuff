8809d6026aa12161ea287e0189991966279b5d6b
jimtheflash <jim.kloet@gmail.com>
2021-03-01 10:34:58 -0600

    update everything with the new script; add some more scraping snippets

--- scratch/scrape nba props.R
+++ scratch/scrape nba props.R
@@ -2,8 +2,8 @@
 # fanduel -----------------------------------------------------------------
 
 # get everything on nba main page
-all_nba = jsonlite::fromJSON('https://il.sportsbook.fanduel.com/cache/psmg/UK/63747.3.json')
-events = all_nba$events
+all_nba <- jsonlite::fromJSON('https://il.sportsbook.fanduel.com/cache/psmg/UK/63747.3.json')
+events <- all_nba$events
 ## after exploring this, doesn't look to have the props, just the spread, 1st half spread, ML, total (which, not the end of the world!)
 
 ## NOT RUN
@@ -17,19 +17,41 @@ events = all_nba$events
 ## looks like each game gets an id, which may be a better way to dig in
 ## how about the event id's being passed to another url
 
-test = jsonlite::fromJSON('https://il.sportsbook.fanduel.com/cache/psevent/UK/1/false/972499.3.json')
-event_market_groups = test$eventmarketgroups
+game_event <- jsonlite::fromJSON('https://il.sportsbook.fanduel.com/cache/psevent/UK/1/false/973064.3.json')
+game_event_market_groups <- game_event$eventmarketgroups
+
+### ok so it's most certainly there's a link between the selectionid in embedded df's and the price, but it's sooooooooo tedious that i'm gonna have to take a pause
+
 
 # drafkings ---------------------------------------------------------------
 
-nba_markets = jsonlite::fromJSON('https://gaming-us-il.draftkings.com//sites/US-IL-SB/api/v1/eventgroup/103/full?includePromotions=true&format=json')
-offer_categories = nba_markets$eventGroup$offerCategories
-game_props = offer_categories[offer_categories$name == "Game Props", ]$offerSubcategoryDescriptors
-offers = game_props[[1]]$offerSubcategory$offers
-for (o in offers) {
-  if (o[[1]]$label != "Next Team to Score - at Score 0-0") {
-    print('nahhh')
-  } else {
-    print('found it')
-  }
+## set a timestamp so we know when these odds were refreshed
+time_stamp <- Sys.time()
+## get the big ol' json from dk - this has all the nba markets
+nba_markets <- jsonlite::fromJSON('https://gaming-us-il.draftkings.com//sites/US-IL-SB/api/v1/eventgroup/103/full?includePromotions=true&format=json')
+## start parsing
+offer_categories <- nba_markets$eventGroup$offerCategories
+## under the game props offer category we want the data.frame with the offer subcats (which is nested, and looks weird if unlisted, so accessing the first element)
+game_props <- offer_categories[offer_categories$name == "Game Props", ]$offerSubcategoryDescriptors[[1]]
+## same deal with the first team to score sub-category
+first_team_to_score <- game_props[game_props$offerSubcategory == "First Team to Score", ]$offerSubcategory$offers[[1]]
+## get all the outcome data.frames into a list for a quick loop
+outcomes <- do.call(rbind, first_team_to_score)$outcomes
+## loop through the outcomes to make a list of data.frames to be combined
+outcomes_df_list <- list()
+for (o in outcomes) {
+  output_df <- data.frame(
+    team = o$label,
+    odds = o$oddsAmerican,
+    opponent = rev(o$label),
+    ts = time_stamp
+  )
+  outcomes_df_list[[length(outcomes_df_list) + 1]] <- output_df
 }
+## stitch together and presto!
+output <- do.call(rbind, outcomes_df_list)
+
+
+# betonline ---------------------------------------------------------------
+
+
