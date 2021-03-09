07853dabec8c762d9cf6bd694d610f5495e6e229
jimtheflash <jim.kloet@gmail.com>
2021-03-09 09:04:38 -0600

    add a bunch of player info, updates to the modeling and vegas scripts

--- nba_lines_scripts/get_nba_lines.R
+++ nba_lines_scripts/get_nba_lines.R
@@ -2,7 +2,7 @@ library(bettoR)
 
 output_dir <- './data/nba_lines/'
 # start_date <- as.Date('2015-01-01')
-start_date <- as.Date('2021-02-01')
+start_date <- as.Date('2016-09-15')
 end_date <- Sys.Date()
 date_range <- gsub('-', '', as.character(seq.Date(start_date, end_date, by = "day")))
 bet_types <- c("spread", "total", "moneyline")
@@ -11,7 +11,7 @@ for (d in date_range) {
   message(Sys.time(), " trying to grab NBA lines for date ", d)
   for (b in bet_types) {
     message("getting ", b)
-    Sys.sleep(3)
+    Sys.sleep(runif(1, min = 1.6, max = 3.2))
     line_info <- try(
       get_lines(
         sport = "NBA",
