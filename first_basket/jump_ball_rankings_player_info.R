ccfc5d1efb5433144fba3726b83e289dff117c44
kylejurczak <kjurczak11@gmail.com>
2021-03-21 10:34:31 -0500

    updated lineups

--- first_basket/jump_ball_rankings_player_info.R
+++ first_basket/jump_ball_rankings_player_info.R
@@ -9,10 +9,10 @@ library(tidyverse)
 # Current model uses last two completed seasons and current season as train data
 # To run model for current day's ratings, train_test_date_split is set to current day
 # Sets furthest date that train data goes back
-earliest_train_data_date <- "2016-09-01"
+earliest_train_data_date <- "2018-09-01"
 # Setting date we want to start logging test data on 
 # Default uses today's date so that all completed games are used in calcualting ratings
-train_test_date_split <- "2018-09-01"
+train_test_date_split <- Sys.Date()
 
 # Read in all player csvs
 file_list <- list.files(path="./data/nba_player_info/")
