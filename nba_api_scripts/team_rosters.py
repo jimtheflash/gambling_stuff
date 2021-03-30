fce4872c20803c6d3becc47ee0ebbcaded3196a7
John Schroeder <4474856+johnlovesdata@users.noreply.github.com>
2021-03-30 15:02:03 -0500

    function for creating single roster file

--- nba_api_scripts/team_rosters.py
+++ nba_api_scripts/team_rosters.py
@@ -1,13 +1,17 @@
 from pathlib import Path
 from time import sleep
+from glob import glob
 from datetime import date
 from random import uniform
 import pandas as pd
 from season import get_current_season, format_season_string
 from team import get_teams, get_team_season_roster
 
+BASE_DATA_PATH = Path(__file__).parent.parent.joinpath("data")
+ARCHIVE = str(date.today()).split("-")
 
-def main() -> bool:
+
+def get_raw_rosters() -> bool:
     teams_df = get_teams()
     sleep(uniform(1.5, 2.5))
     season = get_current_season()
@@ -17,12 +21,11 @@ def main() -> bool:
 
     for team_id in team_ids:
         raw_roster = get_team_season_roster(season_str, team_id)
-        raw_roster_path = Path(__file__).parent.parent.joinpath(
-            "data", "01_raw", "nba_rosters", f"{team_id}.csv.gz"
+        raw_roster_path = Path(BASE_DATA_PATH).joinpath(
+            "01_raw", "nba_rosters", f"{team_id}.csv.gz"
         )
-        today = str(date.today()).split("-")
-        raw_archive_roster_path = Path(__file__).parent.parent.joinpath(
-            "data", "01_raw", "nba_rosters", *today, f"{team_id}.csv.gz"
+        raw_archive_roster_path = Path(BASE_DATA_PATH).joinpath(
+            "01_raw", "nba_rosters", *ARCHIVE, f"{team_id}.csv.gz"
         )
         write_paths = [raw_roster_path, raw_archive_roster_path]
         for write_path in write_paths:
@@ -33,29 +36,46 @@ def main() -> bool:
                 compression="gzip",
             )
         sleep(uniform(1.5, 2.5))
+    return True
 
 
-# def get_clean_active_roster(raw_roster_df: pd.DataFrame):
-#     base_data_path = Path(__file__).parent.parent.joinpath("data")
-#     teams_df = pd.read_csv(
-#         Path(base_data_path).joinpath("01_raw", "nba_teams", "current.csv")
-#     )[["TEAM_ID", "ABBREVIATION"]].rename({"ABBREVIATION": "TEAM_ABBREVIATION"})
-#     clean_df = raw_roster_df.join(teams_df, on="TEAM_ID")
-#     final_cols = [
-#         "TEAM_ID",
-#         "TEAM_ABBREVIATION",
-#         "TEAM_NAME",
-#         "PLAYER_ID",
-#         "PLAYER_NAME",
-#         "PLAYER_SLUG",
-#         "HEIGHT",
-#         "WEIGHT",
-#         "AGE",
-#     ]
-#     # get additional player info
-#     # get team abbreviations
-
-#     pass
+def main() -> bool:
+    # raw_success = get_raw_rosters()
+    teams_df = pd.read_csv(
+        Path(BASE_DATA_PATH).joinpath("01_raw", "nba_teams", "current.csv")
+    )
+    teams = teams_df[["TEAM_ID", "ABBREVIATION"]].rename(
+        columns={"ABBREVIATION": "TEAM_ABBREVIATION"}
+    )
+    raw_roster_files = glob(
+        str(Path(BASE_DATA_PATH).joinpath("01_raw", "nba_rosters", "*.csv.gz"))
+    )
+    rosters_df = pd.concat([pd.read_csv(x) for x in raw_roster_files])
+    rosters = rosters_df.rename(
+        columns={"TeamID": "TEAM_ID", "PLAYER": "PLAYER_NAME", "EXP": "EXPERIENCE"}
+    )
+    clean_df = pd.merge(rosters, teams, on="TEAM_ID")
+    final_cols = [
+        "TEAM_ID",
+        "TEAM_ABBREVIATION",
+        "PLAYER_ID",
+        "PLAYER_NAME",
+        "PLAYER_SLUG",
+        "POSITION",
+        "HEIGHT",
+        "WEIGHT",
+        "AGE",
+        "EXPERIENCE",
+    ]
+    curated_path = Path(BASE_DATA_PATH).joinpath(
+        "02_curated", "nba_rosters", "current.csv.gz"
+    )
+    curated_archive_path = Path(BASE_DATA_PATH).joinpath(
+        "02_curated", "nba_rosters", ARCHIVE[0], ARCHIVE[1], f"{ARCHIVE[2]}.csv.gz"
+    )
+    for path in [curated_path, curated_archive_path]:
+        Path(path).parent.mkdir(parents=True, exist_ok=True)
+        clean_df[final_cols].to_csv(path, index=False, compression="gzip")
 
 
 if __name__ == "__main__":
