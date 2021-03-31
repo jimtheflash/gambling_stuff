3b48f6f63e91f5505b9e87d08ac186c81332e78d
John Schroeder <4474856+johnlovesdata@users.noreply.github.com>
2021-03-31 09:16:08 -0500

    add roster work to daily update

--- do_server_daily_update.sh
+++ do_server_daily_update.sh
@@ -2,9 +2,13 @@ echo "Running daily update"
 git checkout main
 git pull
 
+echo "Sync season game data"
 . .venv/bin/activate && python nba_api_scripts/01_sync_season_data_to_local.py
+echo "Sync team roster data"
+. .venv/bin/activate && python nba_api_scripts/team_rosters.py
 COMMIT_DTTM=`date +"%D %T"`
 echo "Commit DTTM: $COMMIT_DTTM"
 git add data/*/*.csv
+git add data/*/*.csv.gz
 git commit -m "Latest file update: $COMMIT_DTTM"
 git push
