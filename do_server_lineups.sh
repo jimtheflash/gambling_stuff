3d749d0a3166a6c38bbf90ed7bb78d834be93041
John Schroeder <4474856+johnlovesdata@users.noreply.github.com>
2021-04-03 12:18:36 -0500

    add first to score R scripts

--- do_server_lineups.sh
+++ do_server_lineups.sh
@@ -1,11 +1,16 @@
-echo "Running daily update"
+echo "Lineups update"
 git checkout main
 git pull
 
+echo "Rotowire lineups"
 . .venv/bin/activate && python nba_api_scripts/rotowire_lineups.py
+echo "First to score bets"
+Rscript first_basket/'01_first_to_score_bets.R'
 git add data/01_raw/nba_lineups/*/*/*/*/*.html
 git add data/01_raw/nba_lineups/*.html
 git add data/02_curated/nba_lineups/*/*/*/*/*.csv
 git add data/02_curated/nba_lineups/*.csv
+git add data/02_curated/nba_first_to_score/*.csv.gz
+git add data/02_curated/nba_first_to_score/*/*/*/*.csv.gz
 git commit -m "Lineups file update"
 git push
