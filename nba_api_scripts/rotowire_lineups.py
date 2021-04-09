509d1e1687cd4aa9e443c687e37f3420de8134a5
John Schroeder <4474856+johnlovesdata@users.noreply.github.com>
2021-04-09 10:04:48 -0500

    fixing errors due to postponed TORCLE game

--- nba_api_scripts/rotowire_lineups.py
+++ nba_api_scripts/rotowire_lineups.py
@@ -10,6 +10,10 @@ from bs4 import BeautifulSoup
 
 # first party
 from normalize_name import *
+from get_logger import get_logger
+
+
+logger = get_logger(__name__)
 
 
 def lineups_from_matchups_html(matchups_html):
@@ -26,31 +30,35 @@ def lineups_from_matchups_html(matchups_html):
             for x in matchup_teams_html.find_all("a")
         ]
         for team in matchup_teams:
-            abbrev = normalize_name(team.get("TEAM_ABBREVIATION"))
-            home_away = team.get("HOME_AWAY")
-            lineup_raw = [
-                x for x in matchup_html.find_all("ul") if home_away in str(x)
-            ][0]
-            players_raw = [
-                x for x in lineup_raw.find_all("li") if "lineup__player" in str(x)
-            ]
-            lineup_status = lineup_raw.find(
-                "li", attrs={"class": re.compile("lineup__status")}
-            ).get("class")[-1]
-            for idx, player in enumerate(players_raw):
-                player_dict = {
-                    "TEAM_ABBREVIATION": abbrev,
-                    "HOME_AWAY": home_away,
-                    "MATCHUP": matchup,
-                    "LINEUP_DESC": lineup_status if idx <= 4 else None,
-                    "TO_PLAY_DESC": player.get("title"),
-                    "PLAYER_NAME": player.find("a").get("title"),
-                    "NAME_SHORT": player.find("a").text,
-                    "STARTING_POSITION": player.find(
-                        "div", attrs={"class", "lineup__pos"}
-                    ).text,
-                }
-                lineup_players_list.append(player_dict)
+            try:
+                abbrev = normalize_name(team.get("TEAM_ABBREVIATION"))
+                home_away = team.get("HOME_AWAY")
+                lineup_raw = [
+                    x for x in matchup_html.find_all("ul") if home_away in str(x)
+                ][0]
+                players_raw = [
+                    x for x in lineup_raw.find_all("li") if "lineup__player" in str(x)
+                ]
+                lineup_status = lineup_raw.find(
+                    "li", attrs={"class": re.compile("lineup__status")}
+                ).get("class")[-1]
+                for idx, player in enumerate(players_raw):
+                    player_dict = {
+                        "TEAM_ABBREVIATION": abbrev,
+                        "HOME_AWAY": home_away,
+                        "MATCHUP": matchup,
+                        "LINEUP_DESC": lineup_status if idx <= 4 else None,
+                        "TO_PLAY_DESC": player.get("title"),
+                        "PLAYER_NAME": player.find("a").get("title"),
+                        "NAME_SHORT": player.find("a").text,
+                        "STARTING_POSITION": player.find(
+                            "div", attrs={"class", "lineup__pos"}
+                        ).text,
+                    }
+                    lineup_players_list.append(player_dict)
+            except:
+                logger.warning(f"Skipping {team}")
+                continue
     return lineup_players_list
 
 
