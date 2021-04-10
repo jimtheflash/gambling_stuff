989f0f3751e60db44b851d80022c730d518d3e34
kylejurczak <kjurczak11@gmail.com>
2021-04-10 14:06:32 -0500

    added exception for dennis smith jr

--- first_basket/01_first_to_score_bets.R
+++ first_basket/01_first_to_score_bets.R
@@ -29,7 +29,8 @@ player_name_changes <-
   add_row(player_api = "Marcus Morris Sr.", player_lineup = "Marcus Morris") %>%
   add_row(player_api = "Danuel House Jr.", player_lineup = "Danuel House") %>%
   add_row(player_api = "Tim Hardaway Jr.", player_lineup = "Tim Hardaway") %>%
-  add_row(player_api = "Wendell Carter Jr.", player_lineup = "Wendell Carter")
+  add_row(player_api = "Wendell Carter Jr.", player_lineup = "Wendell Carter") %>%
+  add_row(player_api = "Dennis Smith Jr.", player_lineup = "Dennis Smith")
 
 ## List of teams playing today for a join
 list_of_team_abbrev_id <-
