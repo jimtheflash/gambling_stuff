449b61ddc2bcf029e7c329a847ff1063d8dd0a3f
jimtheflash <jim.kloet@gmail.com>
2021-04-11 09:31:08 -0500

    update get_nba_lines to make it easier to automate

--- nba_lines_scripts/get_nba_lines.R
+++ nba_lines_scripts/get_nba_lines.R
@@ -1,26 +1,35 @@
 library(bettoR)
 
+# set the output directory
 output_dir <- './data/nba_lines/'
-# start_date <- as.Date('2015-01-01')
-start_date <- as.Date('2021-03-27')
-end_date <- Sys.Date()
-date_range <- gsub('-', '', as.character(seq.Date(start_date, end_date, by = "day")))
+
+# specify bet types to scrape
 bet_types <- c("spread", "total", "moneyline")
-message(Sys.time(), " initializing loop")
+
+# 2 ways to figure out which dates to scrape; either parse the filenames in the output_dir to identify the dates with missing outputs between the minimum date in the data and the current date; or specify manually
+existing_files <- list.files(output_dir, pattern = 'total|spread|moneyline')
+date_strings <- gsub('\\D', '', existing_files)
+date_dates <- as.Date(date_strings, format = '%Y%m%d')
+max_date <- max(date_dates)
+start_date <- max_date + 1
+## or just set it manually
+# start_date <- as.Date('2015-01-01')
+## end_date always the same
+end_date <- Sys.Date()
+## make range of dates the right format for scraping
+date_range <- gsub('-', '', as.character(seq.Date(start_date, end_date, by = "day")))
+
 for (d in date_range) {
-  message(Sys.time(), " trying to grab NBA lines for date ", d)
   for (b in bet_types) {
-    message("getting ", b)
     Sys.sleep(runif(1, min = 1.6, max = 3.2))
     line_info <- try(
       get_lines(
         sport = "NBA",
         bet_type = b,
         start_date = d
-      )
+      ), silent = TRUE
     )
-    if ('try-error' %in% class(line_info)) {
-      message(Sys.time(), " something went amiss trying to scrape ", b, " for date ", d)
+    if (inherits(line_info, 'try-error')) {
       next
     } else {
       output_string <- paste0(output_dir, 'nba_', d, '_', b, '.csv')
